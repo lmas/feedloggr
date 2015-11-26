3a63227d0e5886d102a2ad25c360fb5cae4da30e
A. Svensson <lmasvensson@gmail.com>
2015-11-26 20:02:38 +0100

    Disabled the automaking config code.

--- feedloggr2.go
+++ feedloggr2.go
@@ -4,17 +4,17 @@ import "github.com/lmas/feedloggr2/src"
 
 func main() {
 	// TODO: make example config
-	feeds := []feedloggr2.FeedConfig{
-		feedloggr2.FeedConfig{"reddit - Front", "https://reddit.com/.rss"},
-		feedloggr2.FeedConfig{"reddit - Funny", "https://reddit.com/r/funny.rss"},
-	}
-	c := &feedloggr2.Config{
-		Verbose:    true,
-		Database:   "./tmp/feeds.db",
-		OutputPath: "./tmp/out/",
-		Feeds:      feeds,
-	}
-	feedloggr2.SaveConfig("./tmp/config.json", c)
+	//feeds := []feedloggr2.FeedConfig{
+	//feedloggr2.FeedConfig{"reddit - Front", "https://reddit.com/.rss"},
+	//feedloggr2.FeedConfig{"reddit - Funny", "https://reddit.com/r/funny.rss"},
+	//}
+	//c := &feedloggr2.Config{
+	//Verbose:    true,
+	//Database:   "./tmp/feeds.db",
+	//OutputPath: "./tmp/out/",
+	//Feeds:      feeds,
+	//}
+	//feedloggr2.SaveConfig("./tmp/config.json", c)
 
 	// Load config
 	config, e := feedloggr2.LoadConfig("./tmp/config.json")
