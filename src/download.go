2edbea133dcb4cac093d16f175734ebb57ac49fe
A. Svensson <lmasvensson@gmail.com>
2016-01-13 17:45:48 +0100

    Made it easier to test the feed downloader.

--- src/download.go
+++ src/download.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 
 	rss "github.com/jteeuwen/go-pkg-rss"
+	"github.com/jteeuwen/go-pkg-xmlx"
 )
 
 type FeedDownloader struct {
 	rss   *rss.Feed
 	items []*FeedItem
+	fetch func(string, xmlx.CharsetFunc) error
 }
 
 func NewDownloader(user_agent string) *FeedDownloader {
@@ -16,7 +18,26 @@ func NewDownloader(user_agent string) *FeedDownloader {
 	f := &FeedDownloader{}
 	f.rss = rss.NewWithHandlers(5, false, f, f)
 	f.rss.SetUserAgent(user_agent)
+	f.fetch = f.rss.Fetch
 	return f
+}
+
+// Dummy "download" func for testing purposes
+// TODO: Move to test file?
+func (f *FeedDownloader) dummy_fetch(uri string, charset xmlx.CharsetFunc) error {
+	f.rss.Url = uri
+	items := []*rss.Item{
+		&rss.Item{
+			Title: "1st item",
+			Links: []*rss.Link{&rss.Link{Href: "http://some.link/"}},
+		},
+		&rss.Item{
+			Title: "2st item",
+			Links: []*rss.Link{&rss.Link{Href: "http://some.link2/"}},
+		},
+	}
+	f.ProcessItems(nil, nil, items)
+	return nil
 }
 
 func (f *FeedDownloader) Clear() {
@@ -26,15 +47,15 @@ func (f *FeedDownloader) Clear() {
 func (f *FeedDownloader) DownloadFeed(url string) ([]*FeedItem, error) {
 	// TODO: don't actually download a feed when running tests
 	defer f.Clear()
-	e := f.rss.Fetch(url, nil)
+	e := f.fetch(url, nil)
 	if e != nil {
 		return nil, fmt.Errorf("Error connecting to %s: %s\n", url, e)
 	}
 	return f.items, nil
 }
 
+// Dummy func so go-pkg-rss will run.
 func (f *FeedDownloader) ProcessChannels(feed *rss.Feed, channels []*rss.Channel) {
-	// Dummy func so go-pkg-rss will run.
 }
 
 func (f *FeedDownloader) ProcessItems(feed *rss.Feed, ch *rss.Channel, items []*rss.Item) {
@@ -44,7 +65,6 @@ func (f *FeedDownloader) ProcessItems(feed *rss.Feed, ch *rss.Channel, items []*
 			Url:   it.Links[0].Href,
 			Date:  Now(),
 			Feed:  f.rss.Url,
-			//Feed:  feed.Url, // TODO: fix the feed url thing
 		})
 	}
 }
