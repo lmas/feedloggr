dacc32fe2165c3089f08f2a0e273d1884ff052dd
A. Svensson <github@lmas.se>
2019-05-14 17:15:42 +0200

    Adds user agent in a roundabout way

--- main.go
+++ main.go
@@ -33,6 +33,19 @@ type App struct {
 	feedParser *gofeed.Parser
 }
 
+type UserAgentTransport struct {
+	http.RoundTripper
+}
+
+func (c *UserAgentTransport) RoundTrip(r *http.Request) (*http.Response, error) {
+	// Note: having some issues with reddit not liking bots, so have to
+	// really show them some love with this verbose user agent.
+	// No other way to set it currently, with this rss lib.
+	// See https://github.com/mmcdole/gofeed/issues/74
+	r.Header.Set("User-Agent", "linux:feedloggr:3.0 (by /u/go255)")
+	return c.RoundTripper.RoundTrip(r)
+}
+
 func New(config *Config) (*App, error) {
 	tmpl, err := template.New("page").Parse(tmplPage)
 	if err != nil {
@@ -41,7 +54,8 @@ func New(config *Config) (*App, error) {
 
 	feedParser := gofeed.NewParser()
 	feedParser.Client = &http.Client{
-		Timeout: time.Duration(config.Timeout) * time.Second,
+		Timeout:   time.Duration(config.Timeout) * time.Second,
+		Transport: &UserAgentTransport{http.DefaultTransport},
 	}
 
 	app := &App{
